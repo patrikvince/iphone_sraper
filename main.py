5ef40aa4c843aa44203ee8ad2c190608805338ba
Patesz12 <patrikvince@gmail.com>
2025-09-27 17:27:10 +0200

    make work

--- main.py
+++ main.py
@@ -4,30 +4,24 @@ from scraper import IPhoneScraper
 from excel import MakeExcel
 from dotenv import load_dotenv
 import pandas as pd
-
-
-def list_to_df(list: list):
-    df = pd.DataFrame(list)
-    return df
+from datetime import datetime 
 
 if __name__ == "__main__":
     settings = ScraperSettings(user_agent="MyScraperBot/1.0", timeout=10)
     print(f"User Agent: {settings.user_agent}, Timeout: {settings.timeout}")
     scraper = IPhoneScraper(settings)
+    load_dotenv()
+    link_base = os.getenv("LINK_BASE")
 
-    links = scraper.paginate_links(url="https://www.arukereso.hu/mobiltelefon-c3277/apple/")
+    names, prices = scraper.scrape(link=link_base)
 
-    names, prices = scraper.scrape(links=links)
-    
-    iphone_list = {'Name': [name for sublist in names for name in sublist],
-               'Price': [price for sublist in prices for price in sublist]}
+    df = pd.DataFrame()
+    df["Name"] = [name for sublist in names for name in sublist]
 
-    df = list_to_df(iphone_list)
-    
+    time = datetime.now().strftime("%m/%d/%Y")
+    df[time] = [price for sublist in prices for price in sublist]
+
     load_dotenv()
     path = os.getenv("DOWNLOAD_PATH")
 
-    MakeExcel(path=path).df_to_excel(data_frame=df, header=['Name', 'Price'])
-
-
-    
+    MakeExcel(path=path).df_to_excel(data_frame=df, header=['Name', time])
